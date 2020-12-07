f697f1acfaa590698f1e98ef142da4fd67a4f8a8
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-07 02:34:20 +0000

    Automatically backed up by Learn

---
+++ lib/user.rb
@@ -0,0 +1,25 @@
+class User
+  def initialize
+    @first_name
+    @last_name
+
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+  
+
+end
